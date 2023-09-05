b56ac2f89ecb17b1c166aa5acfd0990555217d81
azdevguide <141618282+azdevguide@users.noreply.github.com>
2023-09-05 16:25:16 +0900

    purge deleted cognitive service account

--- proj15-speech/step-09.sh
+++ proj15-speech/step-09.sh
@@ -1,12 +1,9 @@
 ### Step 9: リソースグループの削除
+
+# Cognitive Servicesアカウント名を取得
+accountname=$(az cognitiveservices account list -g $g --query '[].name' -o tsv|tr -d '\r\n')
+# リソースグループを削除
 az group delete -n $g -y
-
-# Cognitive Serviceアカウントを削除すると「削除されたリソース」となって48時間残るので、こちらも削除する
-# subscriptionId=$(az account show --query id -o tsv|tr -d '\r\n')
-# az rest --method get --header 'Accept=application/json' -u "https://management.azure.com/subscriptions/$subscriptionId/providers/Microsoft.CognitiveServices/deletedAccounts?api-version=2021-04-30" --query 'value[].id' -o tsv|while read -r id; do
-#     id=$(echo $id|tr -d '\r\n')
-#     az rest --method delete --header 'Accept=application/json' -u "https://management.azure.com$id?api-version=2021-04-30"
-# done
-
-# 以下参考 Cognitive Service アカウント リソースをpurge
-# https://learn.microsoft.com/ja-jp/azure/cognitive-services/manage-resources
+# 削除されたCognitive Servicesアカウントが「削除されたリソース」となって48時間残る。それも削除(purge)する。
+sleep 5
+az cognitiveservices account purge -l $l -n "$accountname" -g $g
