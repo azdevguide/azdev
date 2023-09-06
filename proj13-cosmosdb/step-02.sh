a11034ada91d0dc572c801757b3e3bbad1537819
azdevguide <141618282+azdevguide@users.noreply.github.com>
2023-09-06 12:28:48 +0900

    update packages

--- proj13-cosmosdb/step-02.sh
+++ proj13-cosmosdb/step-02.sh
@@ -1,4 +1,4 @@
 ### Step 2: パッケージの追加
 dotnet add package ConsoleAppFramework --version 4.2.4 >> logs/dotnet-new.txt
-dotnet add package Azure.Identity --version 1.9.0 >> logs/dotnet-new.txt
-dotnet add package Microsoft.Azure.Cosmos --version 3.34.0 >> logs/dotnet-new.txt
+dotnet add package Azure.Identity --version 1.10.0 >> logs/dotnet-new.txt
+dotnet add package Microsoft.Azure.Cosmos --version 3.35.3 >> logs/dotnet-new.txt
